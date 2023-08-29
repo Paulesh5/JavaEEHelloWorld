32b504c37e1d4babd53f4e5c200dfa594aeb9e57
Paul Hidalgo <paulesh5@gmail.com>
2023-08-28 22:38:45 -0500

    Deberia funcionar x3 :c

--- src/main/java/com/example/javaeehelloworld/DataSourceProvider.java
+++ src/main/java/com/example/javaeehelloworld/DataSourceProvider.java
@@ -7,9 +7,22 @@ import jakarta.enterprise.inject.Produces;
 
 import javax.sql.DataSource;
 
+@DataSourceDefinition(
+        name = "java:app/jdbc/administrador-sample",
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        databaseName = "ADMINISTRADOR",
+        user = "root",
+        password = "root_bas3",
+        portNumber = 23306,
+        properties = {
+                "allowPublicKeyRetrieval=true",
+                "serverTimezone=America/Lima",
+                "useSSL=false"
+        }
+)
 @ApplicationScoped
 public class DataSourceProvider {
     @Produces
-    @Resource(lookup = "java:app/jdbc/sample")
+    @Resource(lookup = "java:app/jdbc/administrador-sample")
     private DataSource dataSource;
 }
